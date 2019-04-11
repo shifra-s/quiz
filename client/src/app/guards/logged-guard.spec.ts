383f9980b46737636f8ee765a9224fbe2b469b86
shifra <shif602@gmail.com>
2019-04-11 21:51:07 +0300

    login guard

---
+++ client/src/app/guards/logged-guard.spec.ts
@@ -0,0 +1,7 @@
+import { LoggedGuard } from './logged-guard';
+
+describe('LoggedGuard', () => {
+  it('should create an instance', () => {
+    expect(new LoggedGuard()).toBeTruthy();
+  });
+});
---
+++ client/src/app/guards/logged-guard.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from '../services/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggedGuard implements CanActivate {
+
+  constructor(private loginService: LoginService, private router: Router) {
+
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      const logged = this.loginService.isLogged();
+      if (!logged) {
+        this.router.navigate(['login']);
+      }
+      return logged;
+  }
+}
